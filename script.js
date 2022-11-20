153cc8cfa79a95221477be5c70477ee1ff9a7874
Unciaur <52894100+Unciaur@users.noreply.github.com>
2022-11-20 09:06:52 +0000

    Added all essential pages.

---
+++ script.js
@@ -0,0 +1,10 @@
+// var userAnswer = prompt("Do you want to listen to music? (yes/no)");
+// var song = new Audio('arcadia.mp3');
+
+// if (userAnswer === "yes" || "YES" || "Yes" || "YeS") {
+//   alert("Great! Here's some music!");
+//   song.play();
+// } else if (userAnswer === "no" || "NO" || "NO") {
+//   alert("Alright!");
+//   console.log(returnVariable());
+// }
