c8369796c604f5f5ee03bcbda46605ccc6258df9
Unciaur <52894100+Unciaur@users.noreply.github.com>
2022-11-20 16:17:48 +0000

    fix music

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var userAnswer = prompt("Do you want to listen to music? (yes/no)");
-var song = new Audio('arcadia.mp3');
+var song = new Audio('newmusic.mp3');
 
-if (userAnswer === "yes" || "YES" || "Yes" || "YeS") {
+if (userAnswer === "yes" || userAnswer === "Yes" || userAnswer === "YES") {
    alert("Great! Here's some music!");
    song.play();
- } else if (userAnswer === "no" || "NO" || "NO") {
+ } else if (userAnswer === "no" || userAnswer === "No" || userAnswer === "NO") {
    alert("Alright!");
    console.log(returnVariable());
  }
